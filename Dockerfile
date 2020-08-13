52c1273b084566cd10af4245930f5ae7712b2bd5
Rebeca <rebeca@hologram.io>
2020-08-13 07:54:12 -0500

    ay

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,20 @@
-FROM python:3.6.5-alpine
+FROM python:3.6-alpine
+
 WORKDIR /app
+
+COPY requirements.txt /
+
+RUN pip install -r /requirements.txt
+
+COPY . /app
+
+
+FROM python:3.6 as base
+COPY requirements.txt /
+RUN pip wheel --no-cache-dir --no-deps --wheel-dir /wheels -r requirements.txt
+FROM python:3.6-alpine
+COPY --from=base /wheels /wheels
+COPY --from=base requirements.txt .
+RUN pip install --no-cache /wheels/* # flask, gunicorn, pycrypto
+WORKDIR /app
+COPY . /app
--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,31 @@ from flask import Flask, render_template
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('base.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/oops')
+def oops():
+    return render_template('mistakes.html')
+
+@app.route('/engineering')
+def eng():
+    return render_template('engineering.html')
+
+@app.route('/old')
+def old():
+    return render_template('old.html')
+
+@app.route('/values')
+def values():
+    return render_template('values.html')
+
+@app.route('/art')
+def art():
+    return render_template('art.html')
 
 if __name__ == '__main__':
     app.run()
