6a4292d513ee355ec5771009e34c940fcf7f6543
Rebeca <rebeca@hologram.io>
2020-08-17 22:35:12 -0500

    Sorry mom

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template, abort
 from flask_bootstrap import Bootstrap
+from jinja2 import TemplateNotFound
+
+simple_page = Blueprint('simple_page', __name__,
+                        template_folder='templates')
+
+
+@simple_page.route('/', defaults={'page': 'index'})
+@simple_page.route('/<page>')
+
+def show(page):
+    try:
+        return render_template('pages/%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
+
 
 app = Flask(__name__)
+app.register_blueprint(simple_page)
 
 
 def create_app():
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 app = Flask(__name__)
 app.config['TESTING'] = False
+app.config['FREEZER_BASE_URL'] = 'http://borky.xyz/'
