4c9062442991a6d87ed195036c9d5fb255176cb3
Rebeca <rebeca@hologram.io>
2020-08-17 22:43:56 -0500

    Broke it

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from flask_bootstrap import Bootstrap
 from jinja2 import TemplateNotFound
 
 simple_page = Blueprint('simple_page', __name__,
-                        template_folder='templates')
+                        template_folder='templates',
+                        static_folder='static',
+                        static_url_path='http://borky.xyz/')
 
 
-@simple_page.route('/', defaults={'page': 'index'})
+@simple_page.route('/', defaults={'page': 'home'})
 @simple_page.route('/<page>')
+#@simple_page.route('/about', )
 
 def show(page):
     try:
