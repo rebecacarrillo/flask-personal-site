a0b3e0fa60638f92711710e1f699d21762634921
Rebeca <rebeca@hologram.io>
2020-08-18 00:29:59 -0500

    More messing around

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,46 @@
 from flask import Flask, Blueprint, render_template, abort
 from flask_bootstrap import Bootstrap
-from jinja2 import TemplateNotFound
 
-simple_page = Blueprint('simple_page', __name__,
-                        template_folder='templates',
-                        static_folder='static')
-
-
-@simple_page.route('/', defaults={'page': 'home'})
-@simple_page.route('/<page>')
-#@simple_page.route('/about', )
-
-def show(page):
-    try:
-        return render_template('pages/%s.html' % page)
-    except TemplateNotFound:
-        abort(404)
 
 
 app = Flask(__name__)
-app.register_blueprint(simple_page)
+Bootstrap(app)
 
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/oops')
+def oops():
+    return render_template('mistakes.html')
+
+@app.route('/next')
+def nxt():
+    return render_template('next.html')
+
+@app.route('/old')
+def old():
+    return render_template('old.html')
+
+@app.route('/art')
+def art():
+    return render_template('art.html')
+
+@app.route('/values')
+def values():
+    return render_template('values.html')
 
 def create_app():
     with app.app_context():
-        from . import routes  # Import routes
         Bootstrap(app)
         return app
 
